383cff4b2d3fb4e54c8d3e76568d6138d029c213
Vinicius Egidio <me@vinicius.io>
2020-09-18 15:39:14 +0200

    WIP

--- lib/src/banner.dart
+++ lib/src/banner.dart
@@ -2,24 +2,51 @@ import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
-class AppodealBanner extends StatelessWidget {
+class AppodealBanner extends StatefulWidget {
+  @override
+  _AppodealBannerState createState() => _AppodealBannerState();
+}
+
+class _AppodealBannerState extends State<AppodealBanner> {
+  bool isVisible = true;
+  final key = UniqueKey();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 320,
-      height: 50,
-      child: Platform.isIOS
-        ? UiKitView(
-            viewType: 'plugins.io.vinicius.appodeal/banner',
-            creationParams: {},
-            creationParamsCodec: const StandardMessageCodec(),
-          )
-        : AndroidView(
-            viewType: 'plugins.io.vinicius.appodeal/banner',
-            creationParams: {},
-            creationParamsCodec: const StandardMessageCodec(),
-          ),
+    return VisibilityDetector(
+      key: key,
+      onVisibilityChanged: (info) {
+        print(info.visibleFraction);
+        if (mounted) setState(() => isVisible = info.visibleFraction == 1);
+      },
+      child: Container(
+        width: 320,
+        height: 50,
+        child: isVisible ? _PlatformView() : Container(width: 1, height: 1)
+      ),
     );
+  }
+}
+
+class _PlatformView extends StatelessWidget {
+  static const MethodChannel _channel = const MethodChannel('appodeal_flutter');
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS ?
+      UiKitView(
+        viewType: 'plugins.io.vinicius.appodeal/banner',
+        creationParams: {},
+        creationParamsCodec: const StandardMessageCodec(),
+        onPlatformViewCreated: (_) => _channel.invokeMethod('show', { 'adType': 1 }),
+      ) :
+      AndroidView(
+        viewType: 'plugins.io.vinicius.appodeal/banner',
+        creationParams: {},
+        creationParamsCodec: const StandardMessageCodec(),
+        onPlatformViewCreated: (_) => _channel.invokeMethod('show', { 'adType': 1 }),
+      );
   }
 }
